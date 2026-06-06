d010d8e0dd6923867a6933d3e54f03c313be2b80
aditya-yadav1176 <iamaditya1176@gmail.com>
2026-06-06 20:58:40 +0530

    Prepare for deployment

--- app.js
+++ app.js
@@ -25,4 +25,8 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
